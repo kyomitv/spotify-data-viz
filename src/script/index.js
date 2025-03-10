1ae1659bf7d170e1bfdce4d7a2fd4621acf043f3
KyomiTeam <happy89thomas89@gmail.com>
2025-03-10 14:38:44 +0100

    premier graph de simon

--- src/script/index.js
+++ src/script/index.js
@@ -1,13 +1,34 @@
 import downloadData from "./downloadData.js";
 import unpack from "./unpack.js";
+import getTrack from "./api/getTrack.js";
+
+import { graphOnTime, getUniqueSongs } from "./simon.js";
 
 async function main() {
   let startTime = new Date();
-  const data = await downloadData(
-    "data/universal_top_spotify_songs_by_country.csv"
-  );
+  const data = await downloadData("data/spotify_data.csv");
 
   console.log(data, `Data downloaded in ${new Date() - startTime} ms`);
+
+  const track = await getTrack(data[0].spotify_id);
+  console.log(track);
+
+  graphOnTime(data.filter((row) => row.artists.includes("Bruno Mars")));
+
+  const songs = getUniqueSongs(
+    data.filter((row) => row.artists.includes("Bruno Mars"))
+  );
+  console.log(songs);
+  songs.forEach(async (song) => {
+    const track = await getTrack(song.spotify_id);
+    document.getElementById(
+      "songsList"
+    ).innerHTML += `<div id="${song.spotify_id}" class="song"><img src="${track.album.images[2].url}" alt="${song.name}"><div><p>${song.name}</p><p>${song.artists}</p></div></div>`;
+
+    document.getElementById().addEventListener("click", () => {
+      graphOnTime(data.filter((row) => row.spotify_id === song.spotify_id));
+    });
+  });
 }
 
 main();
