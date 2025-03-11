2c3f8f497e54e714cc9cbd97dda3c17268e6403d
KyomiTeam <happy89thomas89@gmail.com>
2025-03-11 18:04:51 +0100

    fix max merge

--- src/script/index.js
+++ src/script/index.js
@@ -4,81 +4,83 @@ import listeChanson from "./listeChanson.js";
 import { graphmap, getdate } from "./map.js";
 import getTrack from "./api/getTrack.js";
 
-import {maxence} from "./maxence.js";
+import { maxence } from "./maxence.js";
 
 import { graphOnTime, getUniqueSongs, barChart } from "./simon.js";
 
 const selection = document.getElementById("choixdate");
 
-
 async function main() {
-    let startTime = new Date();
-    const data = await downloadData("data/spotify_data.csv");
-    const uniqueSongs = listeChanson(data);
-    console.log(uniqueSongs);
-    const selector = document.getElementById("choixChanson");
-    uniqueSongs.forEach((song) => {
-        selector.innerHTML += `<option value="${song}">${song}</option>`;
+  let startTime = new Date();
+  const data = await downloadData("data/spotify_data.csv");
+  const uniqueSongs = listeChanson(data);
+  console.log(uniqueSongs);
+  const selector = document.getElementById("choixChanson");
+  uniqueSongs.forEach((song) => {
+    selector.innerHTML += `<option value="${song}">${song}</option>`;
+  });
+
+  // Ajouter un gestionnaire d'événements pour la sélection de chanson
+  selector.addEventListener("change", (event) => {
+    const selectedSong = event.target.value;
+    const filteredData = data.filter(
+      (row) => row.name === selectedSong && row.artists.includes("Bruno Mars")
+    );
+    filteredData.forEach((row) => {
+      row.date = new Date(row.snapshot_date);
     });
+    filteredData.sort((a, b) => a.date - b.date);
+    maxence(filteredData);
+  });
 
-    // Ajouter un gestionnaire d'événements pour la sélection de chanson
-    selector.addEventListener("change", (event) => {
-        const selectedSong = event.target.value;
-        const filteredData = data.filter((row) => row.name === selectedSong && row.artists.includes("Bruno Mars"));
-        filteredData.forEach((row) => {
-            row.date = new Date(row.snapshot_date);
-        });
-        filteredData.sort((a, b) => a.date - b.date);
-        maxence(filteredData);
-    });
+  maxence(data.filter((row) => row.artists.includes("Bruno Mars")));
 
-    maxence(data.filter((row) => row.artists.includes("Bruno Mars")));
-
-
-    console.log(data, `Data downloaded in ${new Date() - startTime} ms`);
+  console.log(data, `Data downloaded in ${new Date() - startTime} ms`);
 
   graphmap(data, "2024-11-03");
   selection.addEventListener("change", () => {
     graphmap(data, selection.value);
   });
 
+  graphmap(
+    data.filter(
+      (row) =>
+        row.artists.includes("Bruno Mars") &&
+        row.name.includes("Die With A Smile") &&
+        row.snapshot_date.includes("2024-11-03")
+    )
+  );
 
-    graphmap(
-        data.filter(
-            (row) =>
-                row.artists.includes("Bruno Mars") &&
-                row.name.includes("Die With A Smile") &&
-                row.snapshot_date.includes("2024-11-03")
-        )
-    );
+  const track = await getTrack(data[0].spotify_id);
+  console.log(track);
 
-    const track = await getTrack(data[0].spotify_id);
-    console.log(track);
+  graphOnTime(data.filter((row) => row.artists.includes("Bruno Mars")));
 
-    graphOnTime(data.filter((row) => row.artists.includes("Bruno Mars")));
+  const songs = getUniqueSongs(
+    data.filter((row) => row.artists.includes("Bruno Mars"))
+  );
+  console.log(songs);
 
-    const songs = getUniqueSongs(
-        data.filter((row) => row.artists.includes("Bruno Mars"))
-    );
-    console.log(songs);
-
-    // SIMON
-    songs.forEach(async (song) => {
+  // SIMON
+  songs.forEach((song) => {
+    setTimeout(
+      async () => {
         const track = await getTrack(song.spotify_id);
         document.getElementById(
-            "songsList"
+          "songsList"
         ).innerHTML += `<div id="${song.spotify_id}" class="song"><img src="${track?.album.images[2].url}" ><div><p>${song.name}</p><p>${song.artists}</p></div></div>`;
 
         document.querySelectorAll(".song").forEach((songDiv) => {
-            songDiv.addEventListener("click", async () => {
-                if (songDiv.id) {
-                    graphOnTime(data.filter((row) => row.spotify_id === songDiv.id));
-                } else {
-                    graphOnTime(data.filter((row) => row.artists.includes("Bruno Mars")));
-                }
-            });
+          songDiv.addEventListener("click", async () => {
+            if (songDiv.id) {
+              graphOnTime(data.filter((row) => row.spotify_id === songDiv.id));
+            } else {
+              graphOnTime(
+                data.filter((row) => row.artists.includes("Bruno Mars"))
+              );
+            }
+          });
         });
-
       },
       window.localStorage.getItem(song.spotify_id) ? 0 : k * 1100
     );
@@ -97,7 +99,6 @@ async function main() {
       true
     )
   );
-
 }
 
 main();
--- src/script/maxence.js
+++ src/script/maxence.js
@@ -1,107 +1,106 @@
-import  unpack  from "./unpack.js";
+import unpack from "./unpack.js";
 
 export function maxence(rows) {
-    const monthlyData = {};
+  const monthlyData = {};
 
-    rows.forEach((row) => {
-      const date = new Date(row.snapshot_date);
-      const monthKey = `${date.getFullYear()}-${String(
-        date.getMonth() + 1
-      ).padStart(2, "0")}`;
-  
-      if (!monthlyData[monthKey]) {
-        monthlyData[monthKey] = {
-          sum: 0,
-          count: 0,
-        };
-      }
-  
-      monthlyData[monthKey].sum += parseInt(row.daily_rank);
-      monthlyData[monthKey].count += 1;
+  rows.forEach((row) => {
+    const date = new Date(row.snapshot_date);
+    const monthKey = `${date.getFullYear()}-${String(
+      date.getMonth() + 1
+    ).padStart(2, "0")}`;
+
+    if (!monthlyData[monthKey]) {
+      monthlyData[monthKey] = {
+        sum: 0,
+        count: 0,
+      };
+    }
+
+    monthlyData[monthKey].sum += parseInt(row.daily_rank);
+    monthlyData[monthKey].count += 1;
+  });
+
+  const months = Object.keys(monthlyData);
+  const averages = months.map(
+    (month) => monthlyData[month].sum / monthlyData[month].count
+  );
+
+  var frames = [];
+  var x = months;
+  var y = averages;
+
+  var n = x.length;
+  for (var i = 0; i < n; i++) {
+    frames[i] = { data: [{ x: [], y: [] }] };
+    frames[i].data[0].x = x.slice(0, i + 1);
+    frames[i].data[0].y = y.slice(0, i + 1);
+  }
+
+  var trace1 = {
+    type: "scatter",
+    mode: "lines+markers",
+    name: "Selected Song",
+    x: x,
+    y: y,
+    line: { color: "1DB9" },
+    marker: { color: "#1DB9" },
+  };
+
+  var data = [trace1];
+
+  var layout = {
+    template: "plotly_dark",
+    plot_bgcolor: "rgba(0,0,0,0)",
+    paper_bgcolor: "rgba(0,0,0,0)",
+    width: 700,
+    height: 500,
+    font: {
+      color: "white",
+    },
+    hovermode: "closest",
+    title: {
+      text: "",
+    },
+    xaxis: {
+      range: [x[0], x[n - 1]],
+      showgrid: false,
+      fixedrange: true,
+    },
+    yaxis: {
+      range: [50, 1],
+      dtick: 10,
+      showgrid: true,
+      fixedrange: true,
+      tickformat: ",.0f",
+    },
+    legend: {
+      orientation: "h",
+      x: 0.5,
+      y: 1.2,
+      xanchor: "center",
+    },
+  };
+
+  var config = {
+    displayModeBar: false,
+  };
+
+  function startAnimation() {
+    Plotly.animate("myDiv", frames, {
+      frame: {
+        duration: 50,
+        redraw: false,
+      },
+      transition: {
+        duration: 50,
+      },
+      mode: "immediate",
     });
-  
-    const months = Object.keys(monthlyData);
-    const averages = months.map(
-      (month) => monthlyData[month].sum / monthlyData[month].count
-    );
+  }
 
-    var frames = [];
-    var x = months;
-    var y = averages;
-    
-    var n = x.length;
-    for (var i = 0; i < n; i++) {
-        frames[i] = { data: [{ x: [], y: [] }] };
-        frames[i].data[0].x = x.slice(0, i + 1);
-        frames[i].data[0].y = y.slice(0, i + 1);
-    }
-
-    var trace1 = {
-        type: "scatter",
-        mode: "lines+markers",
-        name: "Selected Song",
-        x: x,
-        y: y,
-        line: { color: "1DB9" },
-        marker: { color: "#1DB9" },
-    };
-
-    var data = [trace1];
-
-    var layout = {
-        template: "plotly_dark",
-        plot_bgcolor: "rgba(0,0,0,0)",
-        paper_bgcolor: "rgba(0,0,0,0)",
-        width: 1000,
-        height: 800,
-        font: {
-          color: "white",
-        },
-        hovermode: "closest",
-        title: {
-            text: "",
-        },
-        xaxis: {
-            range: [x[0], x[n - 1]],
-            showgrid: false,
-            fixedrange: true,
-        },
-        yaxis: {
-            range: [50, 1],
-            dtick: 10,
-            showgrid: true,
-            fixedrange: true,
-            tickformat: ",.0f",
-        },
-        legend: {
-            orientation: "h",
-            x: 0.5,
-            y: 1.2,
-            xanchor: "center",
-        }
-    };
-
-    var config = {
-        displayModeBar: false,
-    };
-
-    function startAnimation() {
-        Plotly.animate("myDiv", frames, {
-            frame: {
-                duration: 50,
-                redraw: false,
-            },
-            transition: {
-                duration: 50,
-            },
-            mode: 'immediate'
-        })
-    }
-
-    Plotly.newPlot("myDiv", data, layout, config)
-        .then(function() {
-            Plotly.addFrames("myDiv", frames);
-            // Démarrer l'animation initiale
-            setTimeout(startAnimation, 100);
-        });
+  Plotly.newPlot("myDiv", data, layout, config).then(function () {
+    Plotly.addFrames("myDiv", frames);
+    // Démarrer l'animation initiale
+    setTimeout(startAnimation, 100);
+  });
 }
